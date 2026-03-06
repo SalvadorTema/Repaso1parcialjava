bff08bc08d179d3b04c47b8add27f840c46b72fd
Salvador <stemas@miumg.edu.gt>
2026-03-05 19:13:29 -0800

    Agregando el tercer ejercicio

--- Repaso1parcialjava.java
+++ Repaso1parcialjava.java
@@ -15,6 +15,7 @@ public class Repaso1parcialjava{
             System.out.println("========================================");
             System.out.println("2. Factorial de n (!n)");
             System.out.println("1. Suma de divisibles entre 4 (1 a n)");
+            System.out.println("3. Contador Positivos, Negativos y Ceros");
 
             System.out.println("0. Salir");
             System.out.print("\nElija una opción: ");
@@ -24,6 +25,7 @@ public class Repaso1parcialjava{
             switch (opcion) {
                 case 1: sumarDivisibles4(sc); break;
                 case 2: calcularFactorial(sc); break;
+                case 3: contarTiposNumeros(sc); break;
                 case 0: System.out.println("Saliendo... ¡Buen trabajo!"); break;
                 default: System.out.println("Opción inválida.");
             }
@@ -53,6 +55,20 @@ public class Repaso1parcialjava{
             factorial *= i;
         }
         System.out.println("El factorial de " + n + " es: " + factorial);
+    }
+  //EJERCICIO 3
+    public static void contarTiposNumeros(Scanner sc) {
+        System.out.print("¿Cuántos números va a ingresar (n)?: ");
+        int n = sc.nextInt();
+        int pos = 0, neg = 0, cero = 0;
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Ingrese número " + i + ": ");
+            double num = sc.nextDouble();
+            if (num > 0) pos++;
+            else if (num < 0) neg++;
+            else cero++;
+        }
+        System.out.println("\nResultados:\nPositivos: " + pos + "\nNegativos: " + neg + "\nCeros: " + cero);
     }
 
 }
