fb96023a64a3a1f31d3c2b1c56b19387b49ffa61
Salvador <stemas@miumg.edu.gt>
2026-03-05 18:55:45 -0800

    Primer ejercicio

---
+++ Repaso1parcialjava.java
@@ -0,0 +1,45 @@
+package ejercicios; // Asegúrate que este nombre coincida con el que pusiste en "Package"
+
+import java.util.Scanner;
+
+public class Repaso1parcialjava{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+        	//Menu Enumerado
+            System.out.println("\n========================================");
+            System.out.println("   MENÚ DE EJERCICIOS 1-20");
+            System.out.println("========================================");
+            System.out.println("1. Suma de divisibles entre 4 (1 a n)");
+
+            System.out.println("0. Salir");
+            System.out.print("\nElija una opción: ");
+            
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1: sumarDivisibles4(sc); break;
+                case 0: System.out.println("Saliendo... ¡Buen trabajo!"); break;
+                default: System.out.println("Opción inválida.");
+            }
+        } while (opcion != 0);
+
+        sc.close();
+    }
+
+    // --- MÉTODOS DE LOS EJERCICIOS DEL 1 AL 5 ---
+    //EJERCICIO 1 
+
+    public static void  sumarDivisibles4(Scanner sc) {
+        System.out.print("Ingrese el valor de n: ");
+        int n = sc.nextInt();
+        int suma = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 4 == 0) suma += i;
+        }
+        System.out.println("La suma de los divisibles entre 4 es: " + suma);
+    }
+}
